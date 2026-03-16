12ada0c05977c0df9126dc9ae0fe2ea592aee5a9
sabil <macbook@kuma.local>
2026-03-16 16:40:04 +0700

    initial commit, admin ui scaffolding

---
+++ client/src/App.tsx
@@ -0,0 +1,172 @@
+import { useState } from "react";
+import { Calculator, Database, Menu } from "lucide-react";
+
+const App = () => {
+  const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
+
+  return (
+    <>
+      {/* Topbar */}
+      <div className="fixed h-14 z-40 left-0 shadow-sm bg-white inset-x-0 flex items-center px-2">
+        <div className="flex gap-2 items-end">
+          <button
+            className="cursor-pointer active:scale-90 p-1 bg-slate-100 rounded"
+            onClick={() => setMobileSidebarOpen(!mobileSidebarOpen)}
+          >
+            <Menu size={24} />
+          </button>
+          <h1 className="text-xl font-medium">EStock</h1>
+        </div>
+      </div>
+      {/* End of Topbar */}
+
+      {/* Wrapper */}
+      <div className="flex">
+        {/* Overlay */}
+        <div
+          className={`absolute inset-0 bg-black opacity-30 z-10 ${mobileSidebarOpen ? "block" : "hidden"}`}
+          onClick={() => setMobileSidebarOpen(!mobileSidebarOpen)}
+        />
+        {/* End of Overlay */}
+
+        {/* Sidebar */}
+        <div
+          className={`w-64 p-3 mt-14 h-screen bg-white shadow fixed z-20 ${mobileSidebarOpen ? " md:translate-x-0" : "md:translate-x-0 -translate-x-full"} transition-transform`}
+        >
+          <h1>Menu</h1>
+          <div className="mt-5 flex flex-col gap-1">
+            <a
+              href="#"
+              className="py-2 px-3 flex items-center gap-1 rounded-md hover:text-white hover:bg-red-500 transition-colors"
+            >
+              <Database size={18} />
+              <span>Master Produk</span>
+            </a>
+            <a
+              href="#"
+              className="py-2 px-3 flex items-center gap-1 rounded-md hover:text-white hover:bg-red-500 transition-colors"
+            >
+              <Calculator size={18} />
+              <span>Stok Opname</span>
+            </a>
+          </div>
+        </div>
+        {/* End of Sidebar */}
+
+        {/* Content */}
+        <div className="bg-slate-50 mt-14 md:ml-64 w-full">
+          <div className="p-5">
+            <div className="spa">
+              <h1 className="text-4xl">Master Produk</h1>
+              <small>Kelola data produk di sini</small>
+            </div>
+            <div className="rounded-lg shadow bg-white mt-8 h-full">
+              <div className="p-1 bg-slate-500 rounded-t text-white text-sm">
+                <h1>Tabel Produk</h1>
+              </div>
+              <div className="min-w-full mt-2 p-3">
+                <div className="border border-table-line overflow-x-auto [&::-webkit-scrollbar]:h-2 [&::-webkit-scrollbar-thumb]:rounded-none [&::-webkit-scrollbar-track]:bg-scrollbar-track [&::-webkit-scrollbar-thumb]:bg-scrollbar-thumb">
+                  <table className="min-w-full divide-y divide-table-line">
+                    <thead>
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-start text-xs font-medium text-muted-foreground-1 uppercase"
+                        >
+                          Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-start text-xs font-medium text-muted-foreground-1 uppercase"
+                        >
+                          Age
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-start text-xs font-medium text-muted-foreground-1 uppercase"
+                        >
+                          Address
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-end text-xs font-medium text-muted-foreground-1 uppercase"
+                        >
+                          Action
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-table-line">
+                      <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-foreground">
+                          John Brown
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-foreground">
+                          45
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-foreground">
+                          New York No. 1 Lake Park
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                          <button
+                            type="button"
+                            className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-primary hover:text-primary-hover focus:outline-hidden focus:text-primary-focus disabled:opacity-50 disabled:pointer-events-none"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-foreground">
+                          Jim Green
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-foreground">
+                          27
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-foreground">
+                          London No. 1 Lake Park
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                          <button
+                            type="button"
+                            className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-primary hover:text-primary-hover focus:outline-hidden focus:text-primary-focus disabled:opacity-50 disabled:pointer-events-none"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-foreground">
+                          Joe Black
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-foreground">
+                          31
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-foreground">
+                          Sidney No. 1 Lake Park
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                          <button
+                            type="button"
+                            className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg text-primary hover:text-primary-hover focus:outline-hidden focus:text-primary-focus disabled:opacity-50 disabled:pointer-events-none"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End of Content */}
+      </div>
+      {/* End of Wrapper */}
+    </>
+  );
+};
+
+export default App;
